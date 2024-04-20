7b404a5463f14a402a73e75c551bf9f7278c0e8e
shijo thomas <shijothomashome@gmail.com>
2024-04-20 22:40:46 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
